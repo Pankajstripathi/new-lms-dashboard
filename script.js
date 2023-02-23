958e7673784b65f7294cf79a31f1e8ec7d4ad7b8
Pankajstripathi <pankajtripathi0881@gmail.com>
2023-02-23 19:40:27 +0530

    chnges in both pages

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ var data = {
     label: 'Top Students Department',
     data: [31, 28, 26, 23, 20, 14,8 ],
     backgroundColor: [
-      'rgba(54, 162, 235, 0.8)',
+      'rgb(0, 98, 169)',
         'rgba(244, 197, 75, 0.8)', 
-        'rgba(54, 162, 235, 0.8)',
+        'rgb(0, 98, 169)',
         'rgba(244, 197, 75, 0.8)', 
-        'rgba(54, 162, 235, 0.8)',
+        'rgb(0, 98, 169)',
         'rgba(244, 197, 75, 0.8)', 
     ],
     // borderColor: 'rgba(54, 162, 235, 1)',
@@ -71,3 +71,39 @@ var myPieChart = new Chart(pieChart, {
 });
 
 
+// progrees bar in card
+let progressBar = document.querySelector('.progress-bar');
+let value = 25; // Change this value to update the progress
+
+progressBar.style.width = value + '%';
+progressBar.setAttribute('aria-valuenow', value);
+
+/// new page
+// Bar chart 1
+var ctx1 = document.getElementById('bar-chart-1').getContext('2d');
+var myChart1 = new Chart(ctx1, {
+  type: 'horizontalBar',
+  data: {
+    labels: ['Chapter1', 'Chapter2', 'Chapter3'],
+    datasets: [{
+      data: [20, 20, 3],
+      backgroundColor: [
+         'rgb(0, 98, 169)',
+        'rgb(79, 183, 52)',
+           'rgb(0, 98, 169)',
+        
+      ],
+      // borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+
+//// bnm,.
